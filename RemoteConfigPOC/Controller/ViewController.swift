2ceb593a3c2c721c6c7f6e83d33da87f76784e46
Veli Bacik <velibacik60@gmail.com>
2019-03-08 15:39:14 +0300

    z

--- RemoteConfigPOC/Controller/ViewController.swift
+++ RemoteConfigPOC/Controller/ViewController.swift
@@ -34,7 +34,9 @@ class ViewController: UIViewController , WKNavigationDelegate {
         })
     }
     override func viewDidAppear(_ animated: Bool) {
-            // UI Updates here for task complete. 
+            // UI Updates here for task complete.
+        
+        
         self.manager.getServiceControl(success: { (result : Bool) in
                 switch result {
                 case true:
